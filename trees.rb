edc3d7d3cc6991963ae35216f553e25f4aa18fd9
OlukaDenis <olukadeno@gmail.com>
2020-07-28 19:25:31 +0300

    Updated trees and binary search

--- trees.rb
+++ trees.rb
@@ -1,15 +1,15 @@
-# frozen_string_literal: true
-
 def leftmost_nodes_sum(array)
-  # your code here
-  sum = 0
-  x = 0
-  while x < array.size
-    sum += array[x]
-    x = ((2 * x) + 1)
-  end
-  sum
+  get_nodes(array, 0, 0)
 end
 
-puts leftmost_nodes_sum([2, 7, 5, 2, 6, 0, 9])
+def get_nodes(array, i, total)
+  while i < array.size
+    total += array[i]
+    i = (2 * i) + 1
+    get_nodes(array, i, total)
+  end
+
+  total
+end
+puts leftmost_nodes_sum([2, 7, 5, 2, 6, 0, 9, 3, 8])
 # => 11
