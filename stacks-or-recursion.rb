f5716d1900ac91257a2bbdf41afda52aaef73167
OlukaDenis <olukadeno@gmail.com>
2020-07-30 11:12:31 +0300

    Added recursive stacks or recursion

--- stacks-or-recursion.rb
+++ stacks-or-recursion.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 def tree_height(tree_as_list)
-  # your code here
-  height = 1
-  i = 1
-  while i < tree_as_list.size
+  find_height(tree_as_list, 1, 1)
+end
 
-    height += 1
-
-    i += 2 * i + 1
+def find_height(tree, i, height)
+  while i < tree.size
+   find_height(tree,  i += 2 * i + 1, height += 1)
   end
   height
 end
+
+puts tree_height([2, 7, 5, 2, 6, 0, 9])
+# => 3
+
+puts tree_height([1, 7, 5, 2, 6, 0, 9, 3, 7, 5, 11, 0, 0, 4, 0])
+# => 4
